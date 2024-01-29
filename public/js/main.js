1543e0c7c84bf1d581ee7a6383145356118c565e
Vlad <vladyslavhrebennikov@gmail.com>
2024-01-29 22:29:17 +0200

    homme

--- public/js/main.js
+++ public/js/main.js
@@ -22,3 +22,39 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
   });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var multiItemCarousel = document.querySelectorAll('.multi-item-carousel');
+
+    multiItemCarousel.forEach(function(carousel) {
+        // Инициализация карусели
+        var bsCarousel = new bootstrap.Carousel(carousel, {
+            interval: false
+        });
+
+        var items = carousel.querySelectorAll('.item');
+
+        // Для каждого слайда в карусели
+        items.forEach(function(item) {
+            var nextItem = item.nextElementSibling;
+            if (!nextItem) {
+                nextItem = carousel.querySelector('.item:first-child');
+            }
+
+            // Клонируем первый дочерний элемент следующего слайда
+            var clonedChild = nextItem.querySelector(':first-child').cloneNode(true);
+            item.appendChild(clonedChild);
+
+            // Проверяем следующий за следующим слайд
+            var nextNextItem = nextItem.nextElementSibling;
+            if (!nextNextItem) {
+                nextNextItem = carousel.querySelector('.item:first-child');
+            }
+
+            // Клонируем первый дочерний элемент следующего за следующим слайда
+            var clonedNextChild = nextNextItem.querySelector(':first-child').cloneNode(true);
+            item.appendChild(clonedNextChild);
+        });
+    });
+});
+
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -54,15 +54,14 @@
         </div>
     </div>
 
-    <h2>Naše služby</h2>
+    <h2 class="text-center">Naše služby</h2>
 
-    <a href="#" class="btn btn-primary">Click</a>
     <div class="home-services-text">
         Opravy vykonávame najmodernejšími technológiami a používame najmodernejšie postupy. Naši technici sú vyškolení a majú bohaté skúsenosti s opravou notebookov a počítačov.
     </div>
 
     <div class="row">
-        <div class="col-lg-4">
+        <div class="col-lg-4 p-5">
             <div class="home-sevice-img">
                 <img class="image" src="{{ asset('services/service-01.jpg') }}" alt="">
             </div>
@@ -72,7 +71,7 @@
             </div>
             <a href="#" class="btn btn-primary">Click</a>
         </div>
-        <div class="col-lg-4">
+        <div class="col-lg-4 p-5">
             <div class="home-sevice-img">
                 <img class="image" src="{{ asset('services/service-02.jpg') }}" alt="">
             </div>
@@ -82,7 +81,7 @@
             </div>
             <a href="#" class="btn btn-primary">Click</a>
         </div>
-        <div class="col-lg-4">
+        <div class="col-lg-4 p-5">
             <div class="home-sevice-img">
                 <img class="image" src="{{ asset('services/service-03.jpg') }}" alt="">
             </div>
@@ -92,7 +91,7 @@
             </div>
             <a href="#" class="btn btn-primary">Click</a>
         </div>
-        <div class="col-lg-4">
+        <div class="col-lg-4 p-5">
             <div class="home-sevice-img">
                 <img class="image" src="{{ asset('services/service-04.jpg') }}" alt="">
             </div>
@@ -102,7 +101,7 @@
             </div>
             <a href="#" class="btn btn-primary">Click</a>
         </div>
-        <div class="col-lg-4">
+        <div class="col-lg-4 p-5">
             <div class="home-sevice-img">
                 <img class="image" src="{{ asset('services/service-05.jpg') }}" alt="">
             </div>
@@ -112,7 +111,7 @@
             </div>
             <a href="#" class="btn btn-primary">Click</a>
         </div>
-        <div class="col-lg-4">
+        <div class="col-lg-4 p-5">
             <div class="home-sevice-img">
                 <img class="image" src="{{ asset('services/service-06.jpg') }}" alt="">
             </div>
@@ -124,4 +123,181 @@
         </div>
     </div>
 </div>
+
+<div class="home-background-result py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3 text-center">
+                <div class="home-result-logo">
+                    <i class="bi bi-emoji-smile-fill"></i>
+                </div>
+                <div class="home-result-num py-3">
+                    2000
+                </div>
+                <div class="home-result-text">
+                    SPOKOJNÝCH ZÁKAZNÍKOV
+                </div>
+            </div>
+            <div class="col-sm-3 text-center">
+                <div class="home-result-logo">
+                    <i class="bi bi-laptop"></i>
+                </div>
+                <div class="home-result-num py-3">
+                    29000
+                </div>
+                <div class="home-result-text">
+                    OPRAVENÝCH NOTEBOOKOV
+                </div>
+            </div>
+            <div class="col-sm-3 text-center">
+                <div class="home-result-logo">
+                    <i class="bi bi-pc-display"></i>
+                </div>
+                <div class="home-result-num py-3">
+                    13000
+                </div>
+                <div class="home-result-text">
+                    OPRAVENÝCH POČÍTAČOV
+                </div>
+            </div>
+            <div class="col-sm-3 text-center">
+                <div class="home-result-logo">
+                    <i class="bi bi-microsoft"></i>
+                </div>
+                <div class="home-result-num py-3">
+                    9900
+                </div>
+                <div class="home-result-text">
+                    PREINŠTALOVANÝCH OS
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="home-background-comments py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <h2 class="home-comments-title py-2">Čo hovoria naši zákazníci?</h2>
+                <div class="home-comments-text">
+                    Tu sú nejaké recenzie, ktoré nám napísali naši zákazníci
+                    their opinion about the process and the results.
+                </div>
+                <div id="carouselExampleIndicators" class="carousel slide">
+                    <div class="carousel-indicators">
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <div class="home-carousel-comment-text">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, aspernatur illo quaerat perferendis, quo velit ex eius enim modi officia dolorem adipisci provident. Ducimus nesciunt sit cumque quibusdam hic voluptas.
+                        </div>
+                        <div class="home-carousel-customer row">
+                            <div class="home-carousel-customer-img col-lg-2">
+                                <img class="image" src="{{asset('customers/customer1.jpg')}}" alt="">
+                            </div>
+                            <div class="home-carousel-customer-info col-lg-10">
+                                <span class="d-block">
+                                    Name Sername
+                                </span>
+                                <span>
+                                    work position
+                                </span>
+                            </div>
+                        </div>
+                      </div>
+                      <div class="carousel-item">
+                        <div class="home-carousel-comment-text">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, aspernatur illo quaerat perferendis, quo velit ex eius enim modi officia dolorem adipisci provident. Ducimus nesciunt sit cumque quibusdam hic voluptas.
+                        </div>
+                        <div class="home-carousel-customer row">
+                            <div class="home-carousel-customer-img col-lg-2">
+                                <img class="image" src="{{asset('customers/customer2.jpg')}}" alt="">
+                            </div>
+                            <div class="home-carousel-customer-info col-lg-10">
+                                <span class="d-block">
+                                    Name Sername
+                                </span>
+                                <span>
+                                    work position
+                                </span>
+                            </div>
+                        </div>
+                      </div>
+                      <div class="carousel-item">
+                        <div class="home-carousel-comment-text">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, aspernatur illo quaerat perferendis, quo velit ex eius enim modi officia dolorem adipisci provident. Ducimus nesciunt sit cumque quibusdam hic voluptas.
+                        </div>
+                        <div class="home-carousel-customer row">
+                            <div class="home-carousel-customer-img col-lg-2">
+                                <img class="image" src="{{asset('customers/customer3.jpg')}}" alt="">
+                            </div>
+                            <div class="home-carousel-customer-info col-lg-10">
+                                <span class="d-block">
+                                    Name Sername
+                                </span>
+                                <span>
+                                    work position
+                                </span>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                    <button class="carousel-control-prev carousel-link-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next carousel-link-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Next</span>
+                    </button>
+                  </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="gallery-section pt-3">
+        <h2 class="text-center">Naši partneři</h2>
+        <div class="owl-carousel custom-carousel owl-theme">
+        <div class="item-partners-logo active" style="background-image: url('https://source.unsplash.com/300x300/?perth,australia');">
+            <div class="item-desc">
+            {{-- <h3>Coca-cola</h3> --}}
+            {{-- <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo soluta voluptas accusantium in officiis quia ad voluptatem, deleniti quaerat, praesentium temporibus animi ab explicabo saepe? Quis atque obcaecati quasi quo?</p> --}}
+            </div>
+        </div>
+        <div class="item-partners-logo" style="background-image: url('https://source.unsplash.com/300x300/?fremantle,australia');">
+            <div class="item-desc">
+            {{-- <h3>Orlická lodní doprava</h3> --}}
+            {{-- <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos minima vel rerum natus nulla necessitatibus itaque tempore tempora! Porro perferendis temporibus optio iure rem repellendus autem eum debitis similique eaque.</p> --}}
+            </div>
+        </div>
+        <div class="item-partners-logo" style="background-image: url('https://source.unsplash.com/300x300/?west-australia');">
+            <div class="item-desc">
+            {{-- <h3>Platan</h3> --}}
+            {{-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ipsam pariatur cum quis necessitatibus ipsum officia quae, eaque, dolores illo nobis placeat in nam maiores ab a praesentium modi eos!</p> --}}
+            </div>
+        </div>
+        <div class="item-partners-logo" style="background-image: url('https://source.unsplash.com/300x300/?perth');">
+            <div class="item-desc">
+            {{-- <h3>Zavolejsikurýra</h3> --}}
+            {{-- <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem, ex obcaecati? Unde, quidem non. Quam laborum labore maxime quis. Quo optio id recusandae eum mollitia rem ducimus aspernatur blanditiis. Adipisci!</p> --}}
+            </div>
+        </div>
+        <div class="item-partners-logo" style="background-image: url('https://source.unsplash.com/300x300/?margaretriver,australia');">
+            <div class="item-desc">
+            {{-- <h3>Top Cars Lease</h3> --}}
+            {{-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum distinctio corporis suscipit quia aspernatur odit provident tempora. Dicta suscipit ab consectetur aperiam, impedit eos delectus sapiente facere cupiditate consequuntur itaque?</p> --}}
+            </div>
+        </div>
+        </div>
+        </div>
+    </div>
+</div>
+
 @endsection
+
